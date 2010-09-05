d792846551d85d6a8c908ef6e6fcd9fde587f5cf
Mike Park <mikep@quake.net>
2010-09-05 16:07:14 +0200

    initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Praxisbills1::Application.config.secret_token = '03b1b585938fda5fcd3d70fb58344a8f8571a020950577991dbac8652d0fcbaa63cbc1cff7b6aab8b2aa4ba9bf0ad90f2ad61cc71f247ff5fa9696cfc69eaf13'
