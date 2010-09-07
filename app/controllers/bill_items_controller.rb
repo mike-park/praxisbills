1be1308797d9d98eec89f162d617c56be7dd5fe7
Mike Park <mikep@quake.net>
2010-09-07 17:03:29 +0200

    before merge to auth

--- app/controllers/bill_items_controller.rb
+++ app/controllers/bill_items_controller.rb
@@ -1,83 +1,38 @@
 class BillItemsController < ApplicationController
-  # GET /bill_items
-  # GET /bill_items.xml
-  def index
-    @bill_items = BillItem.all
-
-    respond_to do |format|
-      format.html # index.html.erb
-      format.xml  { render :xml => @bill_items }
-    end
-  end
-
-  # GET /bill_items/1
-  # GET /bill_items/1.xml
-  def show
-    @bill_item = BillItem.find(params[:id])
-
-    respond_to do |format|
-      format.html # show.html.erb
-      format.xml  { render :xml => @bill_item }
-    end
-  end
-
-  # GET /bill_items/new
-  # GET /bill_items/new.xml
   def new
-    @bill_item = BillItem.new
-
-    respond_to do |format|
-      format.html # new.html.erb
-      format.xml  { render :xml => @bill_item }
-    end
+    @bill_item = BillItem.new(:auth_id => params[:auth_id])
   end
-
-  # GET /bill_items/1/edit
-  def edit
-    @bill_item = BillItem.find(params[:id])
-  end
-
-  # POST /bill_items
-  # POST /bill_items.xml
+  
   def create
     @bill_item = BillItem.new(params[:bill_item])
-
-    respond_to do |format|
-      if @bill_item.save
-        format.html { redirect_to(@bill_item, :notice => 'Bill item was successfully created.') }
-        format.xml  { render :xml => @bill_item, :status => :created, :location => @bill_item }
-      else
-        format.html { render :action => "new" }
-        format.xml  { render :xml => @bill_item.errors, :status => :unprocessable_entity }
-      end
+    if @bill_item.save
+      redirect_to auth_path(@bill_item.auth_id,
+                            :notice => 'Therapy added')
+    else
+      render 'new'
     end
   end
 
-  # PUT /bill_items/1
-  # PUT /bill_items/1.xml
+  def edit
+    @bill_item = BillItem.find(params[:id])
+    @auth = @bill_item.auth;
+    render 'auths/show'
+  end
+  
   def update
     @bill_item = BillItem.find(params[:id])
-
-    respond_to do |format|
-      if @bill_item.update_attributes(params[:bill_item])
-        format.html { redirect_to(@bill_item, :notice => 'Bill item was successfully updated.') }
-        format.xml  { head :ok }
-      else
-        format.html { render :action => "edit" }
-        format.xml  { render :xml => @bill_item.errors, :status => :unprocessable_entity }
-      end
+    if @bill_item.update_attributes(params[:bill_item])
+      redirect_to(auth_path(@bill_item.auth_id),
+                  :notice => "Updated successfully")
+    else
+      render 'edit'
     end
   end
 
-  # DELETE /bill_items/1
-  # DELETE /bill_items/1.xml
   def destroy
     @bill_item = BillItem.find(params[:id])
     @bill_item.destroy
-
-    respond_to do |format|
-      format.html { redirect_to(bill_items_url) }
-      format.xml  { head :ok }
-    end
+    redirect_to auth_path(@bill_item.auth_id,
+                          :notice => 'Therapy deleted')
   end
 end
